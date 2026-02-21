9f5fd0a19394f7fb8d15fc751d0fb160dd922ad8
Biniam <biniamemshaw84@gmail.com>
2026-02-21 20:33:32 +0300

    update controller

--- src/main/java/com/portfolio/portfolio_backend/controller/ContactController.java
+++ src/main/java/com/portfolio/portfolio_backend/controller/ContactController.java
@@ -11,7 +11,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/contact")
-@CrossOrigin(origins = "http://localhost:5173")
+@CrossOrigin(origins = "${cors.allowed.origins}")
 public class ContactController {
     
     @Autowired
--- src/main/java/com/portfolio/portfolio_backend/controller/ProjectController.java
+++ src/main/java/com/portfolio/portfolio_backend/controller/ProjectController.java
@@ -11,7 +11,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/projects")
-@CrossOrigin(origins = "http://localhost:5173")
+@CrossOrigin(origins = "${cors.allowed.origins}")
 public class ProjectController {
     
     @Autowired
